55418c8efd982807bdaea0fb5230d08815dea09f
CayoP <cayophellipe1@gmail.com>
2023-02-10 22:05:21 -0300

    Estruturas Structures

---
+++ Beecrowd/Estruturas_Structures/1119.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main(){
+    int N, k, m;
+    cin>>N>>k>>m;
+    while(N && k && m){
+
+        bitset<20> cand;
+        //Marca os valores fora do intervalo
+        cand.set(0);
+        for(int i=N+1; i<20; i++){
+            cand.set(i);
+        }
+        ////////////////////////////////////
+        int i=0, j=N+1;
+        int virg=0;
+        while(!cand.all()){
+            for(int cont=0; cont<k;){
+                i++;
+                if(i>N) i=1;
+                if(!cand[i]){
+                    cont++;
+                }
+            }
+            for(int cont=0; cont<m;){
+                j--;
+                if(j<1) j=N;
+                if(!cand[j]){
+                    cont++;
+                }
+            }
+
+            //Imprime vírgula se já saiu algum valor
+            if(virg) cout<<",";
+            virg++;
+
+            if(i!=j){
+                if(i>9)printf(" %d", i);
+                else printf("  %d", i);
+                if(j>9)printf(" %d", j);
+                else printf("  %d", j);
+                cand.set(i);
+                cand.set(j);
+            }
+            else{
+                if(i>9)printf(" %d", i);
+                else printf("  %d", i);
+                cand.set(i);
+            }
+        }
+        cout<<"\n";
+
+        cin>>N>>k>>m;
+    }
+
+    return 0;
+}
